a0ff5bf5e88ec85b86d2a002e0d0b3a295ed7bfe
John <jlane7590@yahoo.ca>
2022-01-16 15:13:16 -0500

    cleaned up unused code, unnecessary comments, and whitespace

--- src/main/java/linkedinlearning/cucumbercourse/BillCalculationHelper.java
+++ src/main/java/linkedinlearning/cucumbercourse/BillCalculationHelper.java
@@ -2,7 +2,7 @@ package linkedinlearning.cucumbercourse;
 
 public class BillCalculationHelper {
 
-    public static double calculateBillForCustomer(double initialBillAmount, Double taxRate){
+    public static double calculateBillForCustomer(Double initialBillAmount, Double taxRate){
         double result = Math.floor((initialBillAmount * taxRate) * 100)/100;
         return result;
     }
